8750e54d95380b7b6924881dbfe4f7608be9f7ca
AllardL <AllardL@users.noreply.github.com>
2019-10-19 16:52:35 +0200

    first commit

---
+++ assets/java/main.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $(".PlayBtn").click(function() {
+        $(".btn-primary").css({'background-color': '#ff0000',
+            'border-color': '#ff0000'});
+    });
+});
